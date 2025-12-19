a929ce0713eb19fecf13334ba7e9258a7914c8fa
anuj781 <anujrana54638@gmail.com>
2025-12-19 21:48:05 +0530

    updated frontend link

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 /* ================= CORS ================= */
 app.use(
   cors({
-    origin: "https://froontend-one.vercel.app/", // 🔒 replace with frontend URL in production
+    origin: "https://froontend-v1dp.vercel.app/", // 🔒 replace with frontend URL in production
     methods: ["GET", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
